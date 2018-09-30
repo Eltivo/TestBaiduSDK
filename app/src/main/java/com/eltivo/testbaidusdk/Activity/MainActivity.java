9252e87a26190d50db29373dee036009475003c6
Yifeng Wang <yifeng.wang@gm.com>
2018-09-30 15:32:11 +0800

    [20180930] add file2

--- app/src/main/java/com/eltivo/testbaidusdk/Activity/MainActivity.java
+++ app/src/main/java/com/eltivo/testbaidusdk/Activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.eltivo.testbaidusdk.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.eltivo.testbaidusdk.R;
@@ -16,7 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView functionList = findViewById(R.id.list_all_function);
-
+        functionList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_all_function_item, getData()));
+        functionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Class<?> targetClass = null;
+                switch(position) {
+                    case 0:
+                        targetClass = TestLocActivity.class;
+                        break;
+                    case 1:
+                        targetClass = TestMapActivity.class;
+                        break;
+                    default:
+                        break;
+                }
+                if (targetClass != null) {
+                    Intent intent = new Intent(MainActivity.this, targetClass);
+                    intent.putExtra("from", 0);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     private List<String> getData() {
---
+++ app/src/main/java/com/eltivo/testbaidusdk/Activity/TestMapActivity.java
@@ -0,0 +1,15 @@
+package com.eltivo.testbaidusdk.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eltivo.testbaidusdk.R;
+
+public class TestMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_map);
+    }
+}
